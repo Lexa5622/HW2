1ac9a3128ef27f6b4e388310de9e48918c38e9bc
Алексей Дорофеев <aleksey.dorofeev@rmg-region.ru>
2024-11-17 22:04:55 +0300

    create variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
     }
 }
